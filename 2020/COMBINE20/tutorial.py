7bd9134e32853eb6b359ea73cc69cc54050acc6a
Tom Mitchell <tcmitchell@users.noreply.github.com>
2020-09-21 15:58:08 -0400

    Add slide 30 to tutorial

--- 2020/COMBINE20/tutorial.py
+++ 2020/COMBINE20/tutorial.py
@@ -9,13 +9,12 @@ import sbol3
 # https://github.com/SynBioDex/Community-Media/blob/master/2020/IWBDA20/SBOL3-IWBDA-2020.pptx
 # ----------------------------------------------------------------------
 
-# TODO: Slide 30 connect to sequence info
 # TODO: Slide 32 for interactions
 # TODO: Slide 34 for constraints
 
 # Define a constant that is not defined in pySBOL3
 SO_ENGINEERED_REGION = sbol3.SO_NS + '0000804'
-
+SO_ASSEMBLY_SCAR = sbol3.SO_NS + '0001953'
 
 # Preamble
 sbol3.set_homespace('https://synbiohub.org/public/igem/')
@@ -67,4 +66,54 @@ doc.add(term)
 i13504.features.append(sbol3.SubComponent(term))
 print(f'Added Terminator SubComponent {term.identity}')
 
+
+# --------------------------------------------------
+# Slide 30: Location of a SubComponent
+# --------------------------------------------------
+
+# BBa_I13504_sequence (875 bp)
+# See https://synbiohub.org/public/igem/BBa_I13504_sequence/1
+seq_13504 = ('aaagaggagaaatactagatgcgtaaaggagaagaacttttcactggagttgtcccaattcttgttgaat'
+             'tagatggtgatgttaatgggcacaaattttctgtcagtggagagggtgaaggtgatgcaacatacggaaa'
+             'acttacccttaaatttatttgcactactggaaaactacctgttccatggccaacacttgtcactactttc'
+             'ggttatggtgttcaatgctttgcgagatacccagatcatatgaaacagcatgactttttcaagagtgcca'
+             'tgcccgaaggttatgtacaggaaagaactatatttttcaaagatgacgggaactacaagacacgtgctga'
+             'agtcaagtttgaaggtgatacccttgttaatagaatcgagttaaaaggtattgattttaaagaagatgga'
+             'aacattcttggacacaaattggaatacaactataactcacacaatgtatacatcatggcagacaaacaaa'
+             'agaatggaatcaaagttaacttcaaaattagacacaacattgaagatggaagcgttcaactagcagacca'
+             'ttatcaacaaaatactccaattggcgatggccctgtccttttaccagacaaccattacctgtccacacaa'
+             'tctgccctttcgaaagatcccaacgaaaagagagaccacatggtccttcttgagtttgtaacagctgctg'
+             'ggattacacatggcatggatgaactatacaaataataatactagagccaggcatcaaataaaacgaaagg'
+             'ctcagtcgaaagactgggcctttcgttttatctgttgtttgtcggtgaacgctctctactagagtcacac'
+             'tggctcaccttcgggtgggcctttctgcgtttata')
+
+i13504_seq = sbol3.Sequence('I13504_sequence')
+i13504_seq.elements = seq_13504
+i13504_seq.encoding = sbol3.SBOL_IUPAC_DNA
+i13504.sequences.append(i13504_seq)
+
+loc = sbol3.Range(i13504_seq, 738, 745)
+i13504_seq_feat = sbol3.SequenceFeature([loc])
+i13504_seq_feat.roles = [SO_ASSEMBLY_SCAR]
+i13504.features.append(i13504_seq_feat)
+
+
+# BBa_B0015_sequence (129 bp)
+# From https://synbiohub.org/public/igem/BBa_B0015_sequence/1
+seq_B0015 = ('ccaggcatcaaataaaacgaaaggctcagtcgaaagactgggcctttcgttttatctgttgtttgtcggt'
+             'gaacgctctctactagagtcacactggctcaccttcgggtgggcctttctgcgtttata')
+
+term_seq = sbol3.Sequence('B0015_sequence')
+term_seq.elements = seq_B0015
+term_seq.encoding = sbol3.SBOL_IUPAC_DNA
+term.sequences.append(term_seq)
+
+# Add the location on to the B0015 SubComponent
+loc = sbol3.Range(i13504_seq, 746, 875)
+for feature in i13504.features:
+    if isinstance(feature, sbol3.SubComponent) and feature.instance_of == term.identity:
+        feature.locations.append(loc)
+
+
+# Write the SBOL to file
 doc.write('gfp.nt', sbol3.SORTED_NTRIPLES)
